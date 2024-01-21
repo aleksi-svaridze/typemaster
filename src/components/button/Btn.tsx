88044e48e1a1655aacbed29c7a9ab507967c81b6
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-01-21 22:24:08 +0400

    Convert style.module.css files to scss partials

--- src/components/button/Btn.tsx
+++ src/components/button/Btn.tsx
@@ -1,8 +1,8 @@
 import { BtnPropsType } from "../../types/Types"
-import styles from './btn.module.css';
 
 export const Btn = (props: BtnPropsType) => {
     return(
-        <button className={`${styles.btn} text-uppercase fw-bolder ${styles.btn__blue}`}>{props.textContent}</button>
+        <button 
+            className='btn text-uppercase fw-bolder btn__blue'>{props.textContent}</button>
     )
 }
