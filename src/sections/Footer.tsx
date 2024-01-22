2a165e5bd893d408940f2eaded1cb888bff60e58
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-01-22 13:02:20 +0400

    Finish Footer Section structure and styling

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,8 +3,12 @@
 
 const FooterSection = () => {
     return(
-        <div>
-            Footer
+        <div className="pt-5 mt-md-5 pb-5">
+            <p className="text-center text-capitalize text-blue-light fs-6 m-0">
+                <span className="fw-bold me-2">Typemaster 2021</span> 
+                |
+                <span className="fw-medium ms-2">all Rights Reserved</span>
+            </p>
         </div>
     )
 }
