0d12d558944f37645205ced7ad72a2ee8a4972a5
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-02-06 14:36:24 +0400

    Add heading and paragraph to About Section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -5,12 +5,18 @@ const AboutSection = () => {
         <div className="tm-container">
             <div className="container">
                 <div className="row">
-                    <div className="col-12 col-lg-8 bg-danger">Hello</div>
-                    <div className="col-12 col-lg-4 bg-primary">
-                        <div className="row">
-                            <div className="col-12 col-md-5 col-lg-12">header</div>
-                            <div className="col-12 col-md-7 col-lg-12">paragraph</div>
-                        </div>
+
+                    <div className="col-12 col-lg-9">Hello</div>
+
+                    <div className="col-12 col-lg-3">
+                        {/* <div className="row text-center text-md-start">
+                            <div className="col-12 col-md-5 col-lg-12">                               */}
+                                <h2 className="about__heading text-uppercase text-blue-dark">mechanical wireless Keyboard</h2>                            
+                            {/* </div> */}
+                            {/* <div className="col-12 col-md-7 col-lg-12"> */}
+                                <p className="about__paragraph mb-0 text-blue-light">The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with reliable wireless connectivity.</p>
+                            {/* </div> */}
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
