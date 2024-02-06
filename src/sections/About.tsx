d257d853a04120c01c3abc747bf3927dd74a49ad
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-02-06 13:12:55 +0400

    Add bootstrap container, row and column styles to About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,8 +2,18 @@
 
 const AboutSection = () => {
     return(
-        <div>
-            About section
+        <div className="tm-container">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-lg-8 bg-danger">Hello</div>
+                    <div className="col-12 col-lg-4 bg-primary">
+                        <div className="row">
+                            <div className="col-12 col-md-5 col-lg-12">header</div>
+                            <div className="col-12 col-md-7 col-lg-12">paragraph</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
